482a0383e00eaa69ac2fa2d7ef6c3f1cec085c46
Shipow <shipowlata@gmail.com>
2014-12-29 10:49:06 +0100

    search params

--- www/js/services.js
+++ www/js/services.js
@@ -12,6 +12,68 @@ angular.module('starter.services', [])
     return storyService;
 })
 
+.factory('settings', function(search) {
+    var settings = {};
+    var settingsService = {};
+    settingsService.set = function(settings) {
+        search.setParams(settings);
+        settings = settings;
+    };
+    settingsService.get = function() {
+        return settings;
+    };
+    return settingsService;
+})
+
+.factory('search', function() {
+    var search = {
+            query: '',
+            params: {}
+        };
+    var searchService = {};
+
+    //dates
+    var last24h = new Date();
+    var pastWeek = new Date();
+    var pastMonth = new Date();
+    last24h = last24h.setDate(last24h.getDate() - 1) / 1000;
+    pastWeek = pastWeek.setDate(pastWeek.getDate() - 7) / 1000;
+    pastMonth = pastMonth.setDate(pastMonth.getDate() - 31) / 1000;
+
+    searchService.setQuery = function(query) {
+        search.query = query;
+    };
+
+    searchService.setParams = function(settings) {
+        if (settings.hasOwnProperty('dateRange')){
+            if (settings.dateRange === 'all'){
+                search.params.numericFilters = '';
+            } else if (settings.dateRange === 'last24h'){
+                search.params.numericFilters = 'created_at_i>' + last24h;
+            } else if (settings.dateRange === 'pastWeek'){
+                search.params.numericFilters = 'created_at_i>' + pastWeek;
+            } else if (settings.dateRange === 'pastMonth'){
+                search.params.numericFilters = 'created_at_i>' + pastMonth;
+            }
+        }
+        if (settings.hasOwnProperty('type')){
+            if (settings.type === 'all'){
+                search.params.tagFilters = '';
+            } else if (settings.type === 'poll' || settings.type === 'comment' || settings.type === 'story') {
+                search.params.tagFilters = settings.type;
+            }
+        }
+        if (settings.hasOwnProperty('sort')){
+            //FIXME
+        }
+    };
+
+    searchService.get = function() {
+        return search;
+    };
+    return searchService;
+})
+
 .filter('moment', function() {
     return function(dateString, format) {
         return moment(dateString * 1000).format(format);
